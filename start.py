ffdfdb05c688a85c988996e38bd07b625d4c2b90
michwilk <michalwilk93@hotmail.com>
2018-04-28 00:22:20 +0200

    modify json

--- start.py
+++ start.py
@@ -1,1 +1,8 @@
-print("hello world")
+# strftime reference and sandbox   --   http://www.strfti.me/
+# %Y-%m-%dT%H:%M:%S
+import json
+
+print("hello")
+with open('input.json') as json_data:
+    d = json.load(json_data)
+print(d) #dict
